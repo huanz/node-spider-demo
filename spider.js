560256ccb099387740e5c9fb98473e1348a01c23
huanz <260540233@qq.com>
2015-06-05 16:09:32 +0800

    :smiley_cat::smiley_cat::smiley_cat:

--- spider.js
+++ spider.js
@@ -16,7 +16,7 @@ function getBookInfo(url, index, callback) {
         .set('X-Real-IP', ipAddress)
         .set('X-Forwarded-For', ipAddress)
         .set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2414.0 Safari/537.36')
-        .set('Cookie', 'bid="MWRYk/7JW0w"; ll="118297"; dbcl2="65043156:cjbfVjCNqBU"; ap=1; ck="9ppw"; push_noty_num=0; push_doumail_num=2; __utmt_douban=1; __utma=30149280.597269190.1430889598.1433399318.1433468754.4; __utmb=30149280.5.10.1433468754; __utmc=30149280; __utmz=30149280.1430892868.2.2.utmcsr=tiye.me|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=30149280.6504; __utmt=1; __utma=81379588.1087632970.1433399354.1433399354.1433468754.2; __utmb=81379588.5.10.1433468754; __utmc=81379588; __utmz=81379588.1433399354.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _pk_id.100001.3ac3=78a4e2cdd1723acd.1433399354.2.1433471658.1433399477.; _pk_ses.100001.3ac3=*')
+        .set('Cookie', '')
         .end(function(err, res) {
             var $ = cheerio.load(res.text);
             var userName = $('#db-usr-profile').find('h1').text().replace(/读过的书\(\d+\)/, '');
@@ -33,7 +33,7 @@ function getBookInfo(url, index, callback) {
                     .set('X-Real-IP', ipAddress2)
                     .set('X-Forwarded-For', ipAddress2)
                     .set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2414.0 Safari/537.36')
-                    .set('Cookie', 'bid="MWRYk/7JW0w"; ll="118297"; dbcl2="65043156:cjbfVjCNqBU"; ap=1; ck="9ppw"; push_noty_num=0; push_doumail_num=2; __utmt_douban=1; __utma=30149280.597269190.1430889598.1433399318.1433468754.4; __utmb=30149280.5.10.1433468754; __utmc=30149280; __utmz=30149280.1430892868.2.2.utmcsr=tiye.me|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=30149280.6504; __utmt=1; __utma=81379588.1087632970.1433399354.1433399354.1433468754.2; __utmb=81379588.5.10.1433468754; __utmc=81379588; __utmz=81379588.1433399354.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _pk_id.100001.3ac3=78a4e2cdd1723acd.1433399354.2.1433471658.1433399477.; _pk_ses.100001.3ac3=*')
+                    .set('Cookie', '')
                     .end(function(err, res2) {
                         var $$ = cheerio.load(res2.text);
                         var des = $$('#link-report').find('.intro').last().text();
